cd13f1e7bee8a142b9d906c46f0c222fd3135747
remihannotel <remi.hannotel@sciencespo.fr>
2021-04-23 16:20:24 +0200

    init

--- replication.jl
+++ replication.jl
@@ -1,2 +1,3 @@
 # Replication of the paper "Disaster risks, disaster strikes, and economic growth: The role of preferences" by T.Douenne (2020).
 # By Rémi Hannotel and Aurélien Salas, for the Numerical Methods class.
+#
