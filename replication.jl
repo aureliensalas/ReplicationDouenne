4fc342bde52ecca038bed04df5b7e9324c5a5a8a
aureliensalas <aurelien.salas@sciencespo.fr>
2021-04-23 16:07:46 +0200

    added the into

--- replication.jl
+++ replication.jl
@@ -1,3 +1,2 @@
-# Replication Aurelien Remi 
-# Remi est stupide
-# a line does not physically exist ... 
+# Replication of the paper "Disaster risks, disaster strikes, and economic growth: The role of preferences" by T.Douenne (2020).
+# By Rémi Hannotel and Aurélien Salas, for the Numerical Methods class.
