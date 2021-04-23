bc949ec22e2511b813dcd9221c4a970801d3f767
aureliensalas <aurelien.salas@sciencespo.fr>
2021-04-23 18:40:12 +0200

    last 5 functions

--- replication.jl
+++ replication.jl
@@ -1,2 +1,45 @@
 # Replication of the paper "Disaster risks, disaster strikes, and economic growth: The role of preferences" by T.Douenne (2020).
 # By Rémi Hannotel and Aurélien Salas, for the Numerical Methods class.
+
+# I. Import packages
+using Plots
+
+# functionine functions
+function lucas_measure(e,g,l,w)
+    psi_optimal = psi(e,g,l,w)
+    psi_bau = e*r+(1-e)*(
+        (1-0)*a - (g*(s^2)/2) - l*(1+d-(0)^u) * (1-w^(1-g))/(1-g)
+        )
+    
+    return (psi_optimal/psi_bau)^(1/(1-e)) - 1
+end
+
+function mrt_lambda_gdp(e,g,l,w)
+    group_1 = (u^(u/(1-u))-u^(1/(1-u)))/(1-u)
+    group_2 = ((1-w^(1-g))/((a^u)*(1-g)))^(1/(1-u))
+    group_3 = (1+d)*(1-w^(1-g))/(1-g)
+    
+    return -(1/psi(e,g,l,w))*(
+            l^(u/(1-u)) * group_1 * group_2 - group_3
+            )
+end
+
+function mrt_omega_gdp(e,g,l,w)
+    group_1 = (u^(u/(1-u))-u^(1/(1-u)))/(1-u)
+    group_2 = ((1-w^(1-g))/(a*(1-g)))^(u/(1-u))
+    
+    return -(w^(-g))/psi(e,g,l,w)*(
+            l*(1+d) - l^(1/(1-u)) * group_1 * group_2
+            )
+end
+
+function rho_to_fit_growth(e,g,l,w,growth_target)
+    
+    group_1 = a*(1-tau(e,g,l,w))
+    group_2 = l*(1+d-(tau(e,g,l,w))^u) * (1-w)
+    group_3 = g*(s^2)/2
+    group_4 = l*(1+d-(tau(e,g,l,w))^u) * (1-w^(1-g)) / (1-g)
+
+    return 1/e * (group_1 - group_2 - (1-e)*(group_1 - group_3 - group_4) - growth_target)
+end
+
