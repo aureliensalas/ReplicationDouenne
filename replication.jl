4dc598f80ae1b4200125f084db86db994a5523e2
remihannotel <remi.hannotel@sciencespo.fr>
2021-04-24 15:42:36 +0200

    changes in functions

--- replication.jl
+++ replication.jl
@@ -93,7 +93,45 @@ tb6 = button("Table 6")
 tb7 = button("Table 7")
 f1 = button("Figure 1")
 
-showtb2 = on(n -> println("Salas"),tb2)
+function table2() 
+    for (l,w,scenario) in [(l_1,w_1,"moderate"),(l_2,w_2,"large"),(l_3,w_3,"extreme")]:
+    r = rho_to_fit_growth(e,g,l,w,growth_target)
+    fpsi = psi(e,g,l,w)/a*100
+    ftau = tau(e,g,l,w)*100
+    ftau2 = tau(e,g,l,w)^(u*100)
+    fexp = expected_growth(e,g,l,w)*100
+    fexp2 = l*(1-(tau(e,g,l,w))^u)*(1-w)*100
+    print("For $scenario, disasters:")
+    print("Share of production consumed: $fpsi % ")
+    print("Share of production in risk-mitigation:', $ftau  % ")
+    print("Reduction in prob. of an env. disaster:, $ftau2  % ")
+    print("Expected growth: , $fexp % ")
+    print("Expected aggregate damages from env. dis. (per year): $fexp2 % ")
+    end 
+end 
+
+
+function table3() 
+    for gamma in [1+1e-09, 3, 5, 10]:
+    r = rho_to_fit_growth(e,gamma,l_1,w_1,growth_target)
+    mrt1 = mrt_lambda_gdp(e,gamma,l_1,w_1) 
+    print("with g= $gamma, w= $w_1, and l= $l_1, , $mrt")
+    r = rho_to_fit_growth(e,gamma,l_2,w_2,growth_target)
+    mrt2 = mrt_lambda_gdp(e,gamma,l_2,w_2)
+    print("with g= $gamma, w= $w_2, and l= $l_2, : $mrt2")
+    r = rho_to_fit_growth(e,gamma,l_3,w_3,growth_target)
+    mrt3 = mrt_lambda_gdp(e,gamma,l_3,w_3)
+    print("with g= $gamma, w=, $w_3, and l= $l_3, : $mrt3")
+    end 
+end 
+
+
+
+
+
+
+
+show_tb2 = on(n -> println("Salas"),tb2)
 
 ui = vbox( # put things one on top of the other
     pad(["top"],1.1em,hbox(pad(["left"],1em,tb2),pad(["left"],1em,tb3), pad(["left"],1em,tb4), pad(["left"],1em,tb5), pad(["left"],1em, tb6),pad(["left"],1em, tb7), pad(["left"],1em, f1))),
